4606a17d72871590692213bd73a9b1190b4f7f29
Nicholas <nicklzw97@hotmail.com>
2021-06-26 20:40:42 +0800

    reformat date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  // expanding out date to show as a calendar
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "numeric" });
+  const year = props.date.toLocaleString("en-US", { year: "numeric" });
   return (
     <div className="expense-item">
-      <span>{props.date.toLocaleString()}</span>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <span className="expense-item__price">{props.amount}</span>
