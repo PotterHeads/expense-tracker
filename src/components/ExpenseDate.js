a1ae8b86ebe4b6b6d9a62d38cbc1967399f0694e
Nicholas <nicklzw97@hotmail.com>
2021-06-26 21:04:45 +0800

    add ExpenseDate styling

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,13 +1,15 @@
+import "./ExpenseDate.css";
+
 function ExpenseDate(props) {
   // expanding out date to show as a calendar
   const month = props.date2.toLocaleString("en-US", { month: "long" });
   const day = props.date2.toLocaleString("en-US", { day: "numeric" });
   const year = props.date2.toLocaleString("en-US", { year: "numeric" });
   return (
-    <div>
-      <div>{month}</div>
-      <div>{day}</div>
-      <div>{year}</div>
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day"> {day}</div>
     </div>
   );
 }
