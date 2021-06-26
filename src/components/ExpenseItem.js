9158deb3d7c3975920623656dae648212d1734c1
Nicholas <nicklzw97@hotmail.com>
2021-06-26 22:12:30 +0800

    refactor to remove duplicate css code and add Card

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date2={props.date1}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <span className="expense-item__price">{props.amount}</span>
       </div>
-    </div>
+    </Card>
   );
 }
 
--- src/components/RenderExpense.js
+++ src/components/RenderExpense.js
@@ -1,10 +1,11 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import "./RenderExpense.css";
 
 function RenderExpense(props) {
   return (
     // items refers to expenses in App.js, items is in an object when it is passed from App.js
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         date1={props.items[0].date}
         title={props.items[0].title}
@@ -20,7 +21,7 @@ function RenderExpense(props) {
         title={props.items[2].title}
         amount={props.items[2].amount}
       ></ExpenseItem>
-    </div>
+    </Card>
   );
 }
 
