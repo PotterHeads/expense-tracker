b68be2453ed85137ba323aea037cabb2c4d665de
Nicholas <nicklzw97@hotmail.com>
2021-06-29 00:06:00 +0800

    added filter function for expenses

--- src/components/Expenses/RenderExpense.js
+++ src/components/Expenses/RenderExpense.js
@@ -17,17 +17,26 @@ function RenderExpense(props) {
         currentFilter={filterYear}
         onExpenseFilter={onExpenseFilterHandler}
       ></ExpenseFilter>
-      {props.items.map((expenseData) => {
-        return (
-          <ExpenseItem
-            // added key to ensure that each component is unique
-            key={expenseData.id}
-            date1={expenseData.date}
-            title={expenseData.title}
-            amount={expenseData.amount}
-          ></ExpenseItem>
-        );
-      })}
+      {props.items
+        // using .filter and .map to filter the expenses by year
+        .filter((expenseData) => {
+          return (
+            expenseData.date.toLocaleString("en-US", { year: "numeric" }) ===
+            filterYear
+          );
+        })
+        // .map returns all the array of the year using ExpenseItem component
+        .map((expenseData) => {
+          return (
+            <ExpenseItem
+              // added key to ensure that each component is unique
+              key={expenseData.id}
+              date1={expenseData.date}
+              title={expenseData.title}
+              amount={expenseData.amount}
+            ></ExpenseItem>
+          );
+        })}
     </Card>
   );
 }
