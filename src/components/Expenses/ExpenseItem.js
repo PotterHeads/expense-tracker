3008b05049f01f1bc246ccfcbc173202373d27c4
Nicholas <nicklzw97@hotmail.com>
2021-06-28 23:22:41 +0800

    minor UI changes

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,13 +2,14 @@ import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
+// component to display expenses
 function ExpenseItem(props) {
   return (
     <Card className="expense-item">
       <ExpenseDate date2={props.date1}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <span className="expense-item__price">{props.amount}</span>
+        <span className="expense-item__price">${props.amount}</span>
       </div>
     </Card>
   );
