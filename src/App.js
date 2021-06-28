e7d54dc08adc6416f00a497bc39bc1070caa7b8d
Nicholas <nicklzw97@hotmail.com>
2021-06-28 23:45:10 +0800

    allow users to add in their expenses

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
+import { useState } from "react";
 import RenderExpense from "./components/Expenses/RenderExpense";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+//fake data
+const fake_expenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 function App() {
-  //fake data
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  // have to make it dynamic such that when a new data is received, it will be update expenses
+  // need to use useState to ensure that expense array is updated
+  // newExpense stores the updated array and it should be passed to RenderExpense where it will be shown to the user
+  const [newExpense, setNewExpense] = useState(fake_expenses);
+
   //expenseData from NewExpense.js
   const saveExpenseDataHandler = (expenseData) => {
-    console.log(expenseData);
+    setNewExpense((prevExpense) => {
+      return [expenseData, ...prevExpense];
+    });
   };
   return (
     <div>
       <NewExpense addNewExpenseData={saveExpenseDataHandler}></NewExpense>
-      <RenderExpense items={expenses}></RenderExpense>
+      <RenderExpense items={newExpense}></RenderExpense>
     </div>
   );
 }
