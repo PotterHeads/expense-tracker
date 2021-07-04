92908eea9029032dc014969b211194d6d9cd475c
Nicholas <nicklzw97@hotmail.com>
2021-07-04 11:12:20 +0800

    refactor rendering of expenses to include ExpenseList

--- src/components/Expenses/RenderExpense.js
+++ src/components/Expenses/RenderExpense.js
@@ -1,8 +1,8 @@
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./RenderExpense.css";
 import ExpenseFilter from "../NewExpense/ExpenseFilter";
 import { useState } from "react";
+import ExpenseList from "./ExpenseList";
 
 function RenderExpense(props) {
   const [filterYear, setFilterYear] = useState("2020"); // set default year to 2020
@@ -18,33 +18,13 @@ function RenderExpense(props) {
       filterYear
     );
   });
-
-  // toggle between no expenses found and expenses found using array length
-  let displayParagraph = <p>No expenses found</p>;
-  if (filteredExpense.length > 0) {
-    displayParagraph = filteredExpense
-      // using .filter and .map to filter the expenses by year
-      // .map returns all the array of the year using ExpenseItem component
-      .map((expenseData) => {
-        return (
-          <ExpenseItem
-            // added key to ensure that each component is unique
-            key={expenseData.id}
-            date1={expenseData.date}
-            title={expenseData.title}
-            amount={expenseData.amount}
-          ></ExpenseItem>
-        );
-      });
-  }
-
   return (
     <Card className="expenses">
       <ExpenseFilter
         currentFilter={filterYear}
         onExpenseFilter={onExpenseFilterHandler}
       ></ExpenseFilter>
-      {displayParagraph}
+      <ExpenseList items={filteredExpense}></ExpenseList>
     </Card>
   );
 }
