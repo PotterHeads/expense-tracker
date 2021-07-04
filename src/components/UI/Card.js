4501a0e1e7f13bd8406e707666adb249276de12b
Nicholas <nicklzw97@hotmail.com>
2021-07-05 00:03:33 +0800

    add new comments

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,7 +1,10 @@
 import "./Card.css";
 
 function Card(props) {
+  // able to accept custom classname as well by pointing to external className
   const cards = "card " + props.className;
+  // must have {props.children} if using this method to add classes
+  // children is a reserved name. it is always the content between the opening and closing tag
   return <div className={cards}>{props.children}</div>;
 }
 
